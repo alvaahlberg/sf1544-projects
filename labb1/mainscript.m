896930953a20bd2ee690a2c60547a36e35253708
Alva Ahlberg <alva@n136-p42.eduroam.kth.se>
2025-11-22 15:21:14 +0100

    Hade skrivit en massa fel i matris, funkar typ nu

--- labb1/mainscript.m
+++ labb1/mainscript.m
@@ -1,13 +1,20 @@
+clear
 
 t = input("Hur långt tid går vi över?: ");
-tspan = [0,t];
+tspan = [0.000000001,t];
 n = input("Hur många steg vill du ta?: ");
 v0 = [0;0;0;0];
 
-Result = Euler(@quatercar,tspan, v0,n);
+%result1 = Euler(@quatercar,tspan, v0,n);
+options = odeset('RelTol',1e-6, 'AbsTol', 1e-7, 'Refine', 4);
+[tv,yv] = ode45(@quatercar,tspan,v0,options);
 
-Tid = Result(1,:);
-Z1 = Result(2,:);
-Z2 = Result(3,:);
+kolumn1 = yv(:,1);
 
-plot(Tid,Z1)
+plot(tv,kolumn1)
+
+%Tid = result1(1,:);
+%Z1 = result1(2,:);
+%Z2 = result1(3,:);
+%plot(Tid,Z1)
+
--- labb1/quatercar.m
+++ labb1/quatercar.m
@@ -13,18 +13,19 @@ H = 0.24; %input('Skriv in höjden på guppet (m): ');
 L = 1;    %input('Skriv in längden på guppet (m): ');
 
 
-if t > (L/(vbil*t))
-    h = 0;
-else
+if t < (L/(vbil))
     h = (H/2)*(1-cos(2*pi*vbil*t/L));
+    dh = (H*pi*vbil/L)*(sin(2*pi*vbil*t/L));
+else
+    h = 0;
+    dh = 0;
 end
-
 
 A = [0 0 1 0
     0 0 0 1 
     -(k1/m1) (k1/m1) -(c1/m1) (c1/m1)
-    (k1/m2) -((k1+k2)/m2) (c1/m2) ((c2*h)/m2)];
+    (k1/m2) -((k1+k2)/m2) (c1/m2) -((c1+c2)/m2)];
 
-g = [0;0;0;(((k2*h)/m2)+((c2*h)/m2))];
+g = [0;0;0;(((k2*h)/m2)+((c2*dh)/m2))];
 
 dv = (A*v + g);
