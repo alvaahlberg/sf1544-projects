feec64a9a8f2a72c0c4bcb092eb7fbad6d7ff625
Alva Ahlberg <alva@n136-p42.eduroam.kth.se>
2025-11-22 12:53:33 +0100

    ändrade lite variabelnamn för att vara lika mellan funktionerna

--- labb1/Euler.m
+++ labb1/Euler.m
@@ -1,13 +1,14 @@
-function [tv,yv]=Euler(f,tspan,y0,n)
-%Solve y'=f(t,y) with inital condition y(a)=y0.
-a=tspan(1);
-b=tspan(2);
-h=(b-a)/n;
+function [tv,yv]=Euler(dv,tspan,v0,n)
+%Löser dv = v(t,y) med initialvärde v(t0) = v0
+
+t0=tspan(1);
+tfin=tspan(2);
+h=(tfin-t0)/n;
 
 tv=a+h*(0:n);
 yv=zeros(size(tv));
-yv(1)=y0;
+yv(1)=v0;
 
 for ii=1:n
-    yv(ii+1)=yv(ii)+h*f(tv(ii),yv(ii));
+    yv(ii+1)=yv(ii)+h*dv(tv(ii),yv(ii));
 end
