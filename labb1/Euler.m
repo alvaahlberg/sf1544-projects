fa869f16da7c2e10c694cae971269fec7daeb3e4
Alva Ahlberg <alva@n136-p42.eduroam.kth.se>
2025-11-22 13:29:38 +0100

    yv nu matris och vektorer läggs till kolumnvis

--- labb1/Euler.m
+++ labb1/Euler.m
@@ -1,14 +1,17 @@
-function [tv,yv]=Euler(dv,tspan,v0,n)
+function [yv]=Euler(quatercar,tspan,v0,n)
 %Löser dv = v(t,y) med initialvärde v(t0) = v0
 
 t0=tspan(1);
 tfin=tspan(2);
 h=(tfin-t0)/n;
 
-tv=a+h*(0:n);
-yv=zeros(size(tv));
-yv(1)=v0;
+tv=t0+h*(0:n);
+N = size(tv);
+yv=zeros(4, N(2));
+yv(:,1)=v0;
 
 for ii=1:n
-    yv(ii+1)=yv(ii)+h*dv(tv(ii),yv(ii));
+    yv(:,ii+1)=yv(ii)+h*quatercar(tv(ii),yv(:,ii));
 end
+
+return
