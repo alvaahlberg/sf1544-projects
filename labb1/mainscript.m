479d2b54a77f2a87b89ee53f47e70010b2caf4b5
Alva Ahlberg <alva@n147-p0.eduroam.kth.se>
2025-11-24 18:17:33 +0100

    Allt plottat i samma fönster

--- labb1/mainscript.m
+++ labb1/mainscript.m
@@ -2,6 +2,7 @@ clear
 
 %z1 och z2. 2a)
 t = input("Hur långt tid går vi över?: ");
+h = input("Hur stor steglängd vill du ta? (för Euler): ");
 tspan = [0.000000001,t];
 v0 = [0;0;0;0];
 
@@ -11,25 +12,40 @@ options = odeset('RelTol',1e-6, 'AbsTol', 1e-7, 'Refine', 1);
 z1 = yv(:,1);
 z2 = yv(:,2);
 
-plot(tv,z1,tv,z2);
+subplot(3,1,1)
+plot(tv,z1,tv,z2)
+xlabel('Tid (s)')
+ylabel('Förskjutning (m)')
+legend('z1','z2')
+
+Maxium_z1 = max (z1)
+Maxium_z2 = max (z2)
 
 %Tidslängd. 2b)
-%lengthtv = size(tv);
+lengthtv = size(tv);
 
-%dtv = zeros(lengthtv(1),1);
+dtv = zeros(lengthtv(1),1);
 
-%dvt(1) = tv(1);
+dvt(1) = tv(1);
 
-%for ii=2:lengthtv(1)
- %   dtv(ii) = tv(ii) - tv(ii-1);
-%end
+for ii=2:lengthtv(1)
+    dtv(ii) = tv(ii) - tv(ii-1);
+end
 
-%plot(tv, dtv);
+subplot(3,1,2)
+plot(tv, dtv)
+xlabel('Tid (s)')
+ylabel('Steglängd')
+
 
 %Eulers metod, jämförelse. 2c)
-n = input("Hur många steg vill du ta?: ");
-result1 = Euler(@quatercar,tspan, v0,n);
+result1 = Euler(@quatercar,tspan, v0,h);
 Tid = result1(1,:);
 Z1 = result1(2,:);
 Z2 = result1(3,:);
+
+subplot(3,1,3)
 plot(Tid,Z2,tv,z2)
+xlabel('Tid (s)')
+ylabel('Förskjutning (m)')
+legend('z2 Euler', 'z2 ODE45')
