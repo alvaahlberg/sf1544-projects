40e4b3812768c6f76c673667999a47ba277ba193
Anton <anton.ledberg@gmail.com>
2025-12-01 20:30:03 +0100

    Betydligt snyggare sätt att lösa uppgiften på, va ute och cykla innan lol, interpulerar inte längre utan evaluerar ode45 i alla de punkter som vi får resultat på med vår metod

--- labb1/mainscriptupg4.m
+++ labb1/mainscriptupg4.m
@@ -43,26 +43,28 @@ stegvek = [1; 1/2; 1/4; 1/8]*h2;
 tspanfix = [0.000000001,0.05];
 
 options = odeset('RelTol',1e-9, 'AbsTol', 1e-9, 'Refine', 1);
-[tv,yv] = ode45(@(t,y) quatercar(A_styv,konst,t,y),tspanfix,v0,options);
+sol = ode45(@(t,y) quatercar(A_styv,konst,t,y),tspanfix,v0,options);
 % yv kommer vara mycket längre än result
 figure2 = figure;
 for ii=1:4
     result2 = imp_trap(@(t,y) quatercar(A_styv,konst,t,y), @(t) quaterg(konst,t), tspanfix, A_styv, v0, stegvek(ii,:));
-    
+    y_ny = deval(sol,result2(1,:));
+    diff = abs(result2(3,:)-y_ny(2,:));
+
     %Läs doc för att fatta interp1 har inte gjort än :(
-    y_interpolate = interp1(result2(1,:),result2(3,:),tv);
-    
-    % ska ta skillnaden mellan dem
-    diff = abs(yv(:,2)-y_interpolate);
-    %spotar ut sig NaN, måste hanteras
-    diff(isnan(diff)) = 0;
-    % skriver ut fel
+    % y_interpolate = interp1(result2(1,:),result2(3,:),tv);
+    % 
+    % % ska ta skillnaden mellan dem
+    % diff = abs(yv(:,2)-y_interpolate);
+    % %spotar ut sig NaN, måste hanteras
+    % diff(isnan(diff)) = 0;
+    % % skriver ut fel
     Maxfel = max(diff)
     Minfel = min(diff)
     Avgfel = mean(diff)
     
     subplot (4,1,ii)
-    plot(tv,diff)
+    plot(result2(1,:),diff)
     hold on
 end
  hold off
