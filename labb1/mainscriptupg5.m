65f85b0d5b4c451649a55269bc22f22a3d03b9a6
Anton <anton.ledberg@gmail.com>
2025-12-02 20:50:27 +0100

    Dålig lösning på 5 c)

--- labb1/mainscriptupg5.m
+++ labb1/mainscriptupg5.m
@@ -1,5 +1,39 @@
 clear
 
 % Löser uppgift 5
-kv0 = [5400;135000];
+kv0 = [6000;100000];
 svarvek = newtons_metod(kv0,100);
+
+t = input("Hur lång tid vill du gå över:");
+h = input("Vilket tidsteg vill du ta:");
+
+tspan = [0.000000001,t];
+konst = values;
+A_ref = matrixA(konst);
+v0 = [0;0;0;0];
+
+
+ref_euler = Euler(@(t,y) quatercar(A_ref,konst,t,y),tspan, v0,h);
+% subplot(2,1,1)
+% title("reference Euler")
+% xlabel('Tid (s)')
+% ylabel('Förskjutning (m)')
+% plot(ref_euler(1,:),ref_euler(2,:))
+
+
+konst(3) = svarvek(1,100);
+konst(4) = svarvek(2,100);
+A_ny = matrixA(konst);
+
+ny_euler = Euler(@(t,y) quatercar(A_ny,konst,t,y),tspan, v0,h);
+% subplot(2,1,2)
+% title("Optrimerade k värden")
+% xlabel('Tid (s)')
+% ylabel('Förskjutning (m)')
+% plot(ny_euler(1,:),ref_euler(2,:))
+
+
+plot(ref_euler(1,:),ref_euler(2,:),ny_euler(1,:),ny_euler(2,:))
+xlabel('Tid (s)')
+ylabel('Förskjutning (m)')
+legend('z1 ref-k', 'z1 opt-k')
