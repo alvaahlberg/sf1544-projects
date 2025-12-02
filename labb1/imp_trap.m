81b4fd39e251e60b8c53429b88927dc5253dc0fd
Anton <anton.ledberg@gmail.com>
2025-12-02 09:09:22 +0100

    Lite konstig lösning där jag tar bort sista element

--- labb1/imp_trap.m
+++ labb1/imp_trap.m
@@ -5,7 +5,7 @@ n = (tspan(2)-tspan(1))/h;
 
 tv=tspan(1)+ h*(0:n);
 N = size(tv);
-yv=zeros(4, N(2));
+yv=zeros(4, N(2)-1);
 yv(:,1)=v0;
 I = eye(4);
 invers = inv(2*I - h*A_styv);
@@ -15,7 +15,7 @@ for ii=1:(n-1)
     %yv(:,ii+1) = (2*yv(:,ii) + h*(quatercar(tv(ii),yv(:,ii))) + quaterg(tv(ii+1)))/(2*I-(h*A_styv));
     yv(:,ii+1) = (invers)*(2*yv(:,ii) + h*(quatercar(tv(ii),yv(:,ii)) + quaterg(tv(ii+1))));
 end
-
+tv(end) = [];
 svar = [tv; yv];
 
 return
